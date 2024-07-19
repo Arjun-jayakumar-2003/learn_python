ed1acf72f7046d33d2052aec3a03ca3105a3b997
Arjun Jayakumar <163170283+Kel-Thuzad2003@users.noreply.github.com>
2024-07-19 09:58:00 +0530

    simple program that makes ne sad
    I am saddened every time I write code in Python. I can't believe my curriculum doesn't have Python as a lab.

---
+++ output1.py
@@ -0,0 +1,12 @@
+character_name = "Tom"
+character_age = "50"
+""" Rather than writing names and numerals everytime we want to mention them we can use variables to store 
+the necessary values then write just the variables when the values are needed to be referenced """
+print("There once was a man named " + character_name + ", ")
+print("he was " + character_age + " years old. ")
+
+character_name = "Mike"
+""" Variables values can be changed which would mean that from that point onwards when that variable is 
+mentioned the new value will be referenced and used  """
+print("He really liked the name " + character_name + ", ")
+print("but didn't like being " + character_age + ".")
